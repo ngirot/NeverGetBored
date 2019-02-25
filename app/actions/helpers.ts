aac393022c90c9a1f4f0a5d3c27500a5ab85a25a
Nicolas Girot <ngirot@hotmail.com>
2019-02-26 00:34:08 +0100

    Format code

--- app/actions/helpers.ts
+++ app/actions/helpers.ts
@@ -1,36 +1,40 @@
-import { Action } from 'redux';
+import {Action} from 'redux';
 
-export interface IAction extends Action {}
+export interface IAction extends Action {
+}
+
 export interface IActionWithPayload<T> extends IAction {
-  readonly payload: T;
+    readonly payload: T;
 }
 
 interface IActionCreator<T> {
-  readonly type: string;
-  (payload: T): IActionWithPayload<T>;
+    readonly type: string;
 
-  test(action: IAction): action is IActionWithPayload<T>;
+    (payload: T): IActionWithPayload<T>;
+
+    test(action: IAction): action is IActionWithPayload<T>;
 }
 
 interface IActionCreatorVoid {
-  readonly type: string;
-  (): IAction;
+    readonly type: string;
 
-  test(action: IAction): action is IAction;
+    (): IAction;
+
+    test(action: IAction): action is IAction;
 }
 
 export const actionCreator = <T>(type: string): IActionCreator<T> =>
-  Object.assign((payload: T): any => ({ type, payload }), {
-    type,
-    test(action: IAction): action is IActionWithPayload<T> {
-      return action.type === type;
-    }
-  });
+    Object.assign((payload: T): any => ({type, payload}), {
+        type,
+        test(action: IAction): action is IActionWithPayload<T> {
+            return action.type === type;
+        }
+    });
 
 export const actionCreatorVoid = (type: string): IActionCreatorVoid =>
-  Object.assign((): any => ({ type }), {
-    type,
-    test(action: IAction): action is IAction {
-      return action.type === type;
-    }
-  });
+    Object.assign((): any => ({type}), {
+        type,
+        test(action: IAction): action is IAction {
+            return action.type === type;
+        }
+    });
--- app/store/configureStore.production.ts
+++ app/store/configureStore.production.ts
@@ -1,7 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
-import { routerMiddleware } from 'react-router-redux';
+import {createBrowserHistory} from 'history';
+import {routerMiddleware} from 'react-router-redux';
 import rootReducer from '../reducers';
 
 const history = createBrowserHistory();
@@ -9,8 +9,8 @@ const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
 export = {
-  history,
-  configureStore(initialState: Object | void) {
-    return createStore(rootReducer, initialState, enhancer);
-  }
+    history,
+    configureStore(initialState: Object | void) {
+        return createStore(rootReducer, initialState, enhancer);
+    }
 };
