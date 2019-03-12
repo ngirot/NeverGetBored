6007b53aac8c55f4e552a17545eab87c7fca5d48
Nicolas Girot <ngirot@hotmail.com>
2019-03-12 19:46:31 +0100

    Improve Twitch typing

--- app/utils/api/twitch/Channel.ts
+++ app/utils/api/twitch/Channel.ts
@@ -2,4 +2,5 @@ export default class Channel {
     public status: string;
     public display_name: string;
     public url: string;
+    public logo: string;
 }
--- app/utils/api/twitch/Stream.ts
+++ app/utils/api/twitch/Stream.ts
@@ -3,6 +3,7 @@ import Preview from "./Preview";
 
 export default class Stream {
     public _id: string;
+    public game: string;
     public channel: Channel;
     public preview: Preview;
 }
