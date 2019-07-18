d2519d819979538cca8ab9a6190124dcb23617c6
Nicolas Girot <ngirot@hotmail.com>
2019-07-18 23:26:17 +0200

    Fix sonar alert: remove useless cast

--- app/domain/store/reducers/index.ts
+++ app/domain/store/reducers/index.ts
@@ -1,11 +1,11 @@
-import {combineReducers, Reducer} from 'redux'
+import {combineReducers} from 'redux'
 import {routerReducer as routing} from 'react-router-redux'
 import platformsReducer from "./platformsReducer"
 import PlatformState from "../state/PlatformState"
 
 const rootReducer = combineReducers({
     platform: platformsReducer,
-    routing: routing as Reducer<any>
+    routing: routing
 })
 
 export interface AppState {
