1c9a77c5274da2f9436f1798aae009aa1e262534
Nicolas Girot <ngirot@hotmail.com>
2019-09-06 00:27:51 +0200

    Improve error management on HTTP calls

--- app/infrastructure/api/http/HttpApi.ts
+++ app/infrastructure/api/http/HttpApi.ts
@@ -14,18 +14,39 @@ export default class HttpApi {
     public get<T>(path: string, options?: Options): Promise<T> {
         const localOptions = options ? options : this.options
 
-        return needle('get', this.baseUrl + path, localOptions)
-            .then((response: any) => {
-                return response.body as T
-            })
+        return new Promise<T>((resolve, reject) => {
+            needle('get', this.baseUrl + path, localOptions)
+                .then((response: any) => {
+                    console.log('response', response)
+                    if (this.isValidResponse(response)) {
+                        resolve(response.body as T)
+                    } else {
+                        reject('Erreur ' + response.statusCode + ": " + response.statusMessage)
+                    }
+                })
+                .catch(reject)
+        })
+
     }
 
     public post<T, V>(path: string, payload: T, options?: Options): Promise<V> {
         const localOptions = options ? options : this.options
 
-        return needle('post', this.baseUrl + path, payload, localOptions)
-            .then((response: any) => {
-                return response.body as V
-            })
+        return new Promise<V>((resolve, reject) => {
+            needle('post', this.baseUrl + path, payload, localOptions)
+                .then((response: any) => {
+                    if (this.isValidResponse(response)) {
+                        resolve(response.body as V)
+                    } else {
+                        reject('Erreur ' + response.statusCode + ": " + response.statusMessage)
+                    }
+                })
+                .catch(reject)
+        })
+
+    }
+
+    private isValidResponse(response: any): boolean {
+        return response.statusCode >= 200 && response.statusCode < 300
     }
 }
